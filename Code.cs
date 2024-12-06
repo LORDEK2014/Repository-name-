2c32842e4f4a3a9c554dab5a6019354a3c9c831f
LORDEK2014 <marinins156@gmail.com>
2024-12-07 09:50:27 +1000

    Это чо то такое
    это чо то описание

---
+++ Code.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Code : MonoBehaviour
+{
+    // —алам чел ты чЄ то начал
+    void Start()
+    {
+        print("Hello, World!");
+        
+
+    }
+
+    // мы чЄ то обновили
+    void Update()
+    {
+        
+    }
+}
